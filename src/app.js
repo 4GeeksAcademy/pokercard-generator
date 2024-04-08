9387873bb736d79c49e20a720c7a1cd328381e1f
Marco Luque <101642586+marcoluqueh@users.noreply.github.com>
2024-04-08 22:06:16 +0000

    Implementadas mejoras de limpieza de código

--- src/app.js
+++ src/app.js
@@ -3,33 +3,11 @@ import "bootstrap";
 import "./style.css";
 
 let soundFlip = new Audio("src/assets/sound/240776__f4ngy__card-flip.wav");
-
-window.onload = function() {
-  //write your code here
-  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-  let suit = ["♦", "♥", "♠", "♣"];
-
-  let randomNumber = Math.floor(Math.random() * number.length);
-  let randomSuit = Math.floor(Math.random() * suit.length);
-
-  document.getElementById("suit").innerHTML = suit[randomSuit];
-  document.getElementById("number").innerHTML = number[randomNumber];
-  document.getElementById("reverse-suit").innerHTML = suit[randomSuit];
-
-  if (suit[randomSuit] === "♥" || suit[randomSuit] === "♦") {
-    document.getElementById("suit").classList.add("text-danger");
-    document.getElementById("reverse-suit").classList.add("text-danger");
-  }
-  soundFlip.play();
-};
-
 let button = document.getElementById("randomButton");
+let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let suit = ["♦", "♥", "♠", "♣"];
 
-button.onclick = function() {
-  //write your code here
-  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-  let suit = ["♦", "♥", "♠", "♣"];
-
+function generateCard() {
   let randomNumber = Math.floor(Math.random() * number.length);
   let randomSuit = Math.floor(Math.random() * suit.length);
 
@@ -40,10 +18,12 @@ button.onclick = function() {
   if (suit[randomSuit] === "♥" || suit[randomSuit] === "♦") {
     document.getElementById("suit").classList.add("text-danger");
     document.getElementById("reverse-suit").classList.add("text-danger");
-  }
-  if (suit[randomSuit] === "♠" || suit[randomSuit] === "♣") {
+  } else {
     document.getElementById("suit").classList.remove("text-danger");
     document.getElementById("reverse-suit").classList.remove("text-danger");
   }
   soundFlip.play();
-};
+}
+
+window.onload = generateCard();
+button.addEventListener("click", generateCard);
