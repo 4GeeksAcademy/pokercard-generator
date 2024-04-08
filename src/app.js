60df965ea72d16dd36245b3fc3642a262e57b234
Marco Luque <101642586+marcoluqueh@users.noreply.github.com>
2024-04-08 17:30:38 +0000

    Primera iteración generador cartas de poker

--- src/app.js
+++ src/app.js
@@ -2,10 +2,48 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let soundFlip = new Audio("src/assets/sound/240776__f4ngy__card-flip.wav");
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suit = ["♦", "♥", "♠", "♣"];
+
+  let randomNumber = Math.floor(Math.random() * number.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+
+  document.getElementById("suit").innerHTML = suit[randomSuit];
+  document.getElementById("number").innerHTML = number[randomNumber];
+  document.getElementById("reverse-suit").innerHTML = suit[randomSuit];
+
+  if (suit[randomSuit] === "♥" || suit[randomSuit] === "♦") {
+    document.getElementById("suit").classList.add("text-danger");
+    document.getElementById("reverse-suit").classList.add("text-danger");
+  }
+  soundFlip.play();
+};
+
+let button = document.getElementById("randomButton");
+
+button.onclick = function() {
+  //write your code here
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suit = ["♦", "♥", "♠", "♣"];
+
+  let randomNumber = Math.floor(Math.random() * number.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+
+  document.getElementById("suit").innerHTML = suit[randomSuit];
+  document.getElementById("number").innerHTML = number[randomNumber];
+  document.getElementById("reverse-suit").innerHTML = suit[randomSuit];
+
+  if (suit[randomSuit] === "♥" || suit[randomSuit] === "♦") {
+    document.getElementById("suit").classList.add("text-danger");
+    document.getElementById("reverse-suit").classList.add("text-danger");
+  }
+  if (suit[randomSuit] === "♠" || suit[randomSuit] === "♣") {
+    document.getElementById("suit").classList.remove("text-danger");
+    document.getElementById("reverse-suit").classList.remove("text-danger");
+  }
+  soundFlip.play();
 };
